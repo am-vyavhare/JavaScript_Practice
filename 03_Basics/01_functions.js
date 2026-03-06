cb60b03abcfb3ea0f64035a5ff8df5fdd6831d25
Aniket Vyavhare <146797997+am-vyavhare@users.noreply.github.com>
2026-03-06 11:15:34 +0000

    functions added

---
+++ 03_Basics/01_functions.js
@@ -0,0 +1,31 @@
+function funName(){
+    console.log("Hello i am function")
+}
+
+funName()
+
+//this are parameters
+function add(n1, n2){
+    // console.log(n1+n2)
+    return n1+n2
+}
+
+//values passing while calling are arguments 
+const result = add(23,12)
+
+console.log("Result is ",result)
+
+
+//when we dont know the fixed no of parameters then collect it inside array named nums
+function calculateCartPrice(...nums){
+    return nums
+}
+
+console.log(calculateCartPrice(100,200,300))
+
+//first 2 value in var and remaining is in ...nums
+function calculateCartPrice(num1, num2, ...nums){
+    return nums
+}
+
+console.log(calculateCartPrice(100,200,300))
